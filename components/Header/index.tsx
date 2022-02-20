6a6ab03ccdd5baffbeabd70fc67522176fa02f3a
Vitali Karpuk <VKarpuk@inspirebrands.com>
2022-02-20 10:49:28 +0300

    fix

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TelegramIcon from '@material-ui/icons/Telegram';
 import styles from './index.module.css';
-import viber from './viber.png';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import classNames from 'classnames';
 interface IInfoAboutCustomer {
   mobile: string;
   recallMessage: string;
@@ -33,10 +34,19 @@ export default function Header() {
         </span>
         <span>У нас вы можете заказать доставку или забрать в пункте самовывоза.</span>
         <span>Доставка цветов будет осуществляться 5, 6, 7, 8 и 9 марта в удобное для вас время</span>
+        <div className={styles.socialIcons}>
+          {/* <InstagramIcon className={styles.iconHover} style={{ color: '#c13584' }} fontSize="large" /> */}
+          <a href="tg://resolve?domain=VitalikKarpuk">
+            <TelegramIcon className={styles.iconHover} style={{ color: '#0088cc' }} fontSize="large" />
+          </a>
+          <a href="https://api.whatsapp.com/send/?phone=375295080632">
+            <WhatsAppIcon className={styles.iconHover} style={{ color: '#43d854' }} fontSize="large" />
+          </a>
+          <a href="viber://chat?number=%2B375295080632">
+            <span className={classNames(styles.viver, styles.iconHover)} />
+          </a>
+        </div>
       </div>
-      {/* <InstagramIcon style={{ color: '#c13584' }} fontSize="large" />
-      <TelegramIcon style={{ color: '#1f304e' }} fontSize="large" />
-      <span className={styles.viver} /> */}
     </header>
   );
 }
